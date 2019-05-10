46ee19ac1621eed500775e51eaa4c68460357ce6
Kawena <kawenagoss@gmail.com>
2019-05-10 14:18:37 -0700

    Zoo
    Finishing Following Along with SQL ZOO  Videos

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -11,6 +11,20 @@ DECLARE @VAR1 int, @VAR2 int
 SET @var1 = 3
 set @var2 = 5
 
-PRINT 'Having fun with ' + 'TSQL and MS SQL SERVER!'
+PRINT 'Variable 1= ' + CONVERT(varchar(5) + CHAR(13) + 'Variable 2 ' + CONVERT(varchar(5),@var2) + CHAR(13) + 'Total: '
+PRINT @var1 + @var2
 
-PRINT CHAR(9) + 'I have ' + CONVERT(varchar(50),@var1) + CHAR(13) + 'dollars...'
+IF @var1 < 2
+	BEGIN
+		PRINT 'Variable 1 <= ' + CONVERT(varchar(5),@var1) + CHAR(13)
+	END
+ELSE IF @var 1 > 1 AND @var1 < 3
+	BEGIN
+		PRINT 'Variable 1 is NOT ' CONVERT(varchar(5),@var1) + CHAR(13)
+	END
+ELSE IF @var1 = 4 OR  @var 1 < 6
+	BEGIN
+		PRINT 
+
+
+
